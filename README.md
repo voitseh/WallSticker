fc008c29fdca9385db0b6394fe578cb8f026013d
voitseh <voitseh.kaminsky@gmail.com>
2018-01-09 19:31:05 +0200

    first commit

--- README.md
+++ README.md
@@ -73,3 +73,4 @@ Then run the Flask app:
 python app.py
 ```
 
+# WallSticker
--- js/Hello.js
+++ js/Hello.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+    <div>
+      <Paper style={style} zDepth={3} />
+    </div>
+
+);
+
+export default PaperExampleSimple;
+/*
 var Hello = React.createClass({
   render() {
     return <h1>Hello, world</h1>;
